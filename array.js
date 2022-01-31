5c78b9929111f1f0030478c2b6b5555c52ca3f3e
afzal75 <afzalbhuiyan16@gmail.com>
2022-01-31 08:15:53 -0800

    practice.js

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
- var number = [10, 15, 20, 25, 30, 35, 22, 55, 77, 99];
+var number = [10, 15, 20, 25, 30, 35, 22, 55, 77, 99];
 
 //  var addOneNumber = number.push(88); //push er maddhome laste data add kore
 //  console.log(number);
@@ -14,4 +14,3 @@
 // console.log(number.length); //length er maddhome variable a koto gulu element ache ta jana jabe.
 
 console.log(number.indexOf(77));
-
---
+++ practice.js
@@ -0,0 +1,58 @@
+// var number1 = 34;
+// var number2 = 10;
+// var number3 = 88;
+
+
+// if (number1 < number2) {
+//     if (number1 < number2) {
+//         console.log('number1 is small');
+//     }
+// }
+// else if (number2 < number3) {
+//     if (number2 < number1) {
+//         console.log('number2 is small');
+//     }
+// }
+// else {
+//     console.log('number2 is small');
+// }
+
+
+// var input = 't';
+// var letter = input.toLowerCase();
+
+// if (letter =='a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+//     console.log(letter, 'is vowel');
+// }
+// else {
+//     console.log(letter, 'is not vowel')
+// }
+
+// for (var i = 1; i <= 100; i += 2) {
+//    if (i % 2 == 1);
+//     console.log(i);
+// }
+// }
+
+// for (var i = 0; i <=100; i++) {
+//         if (i % 2 == 0) {
+//              console.log(i);
+//         }
+//     }
+
+// var number = 8;
+
+// for (var i = 1; i <= 10; i++) {
+//     var result = number * i;
+//     var muliplicationTables = i + ' * ' + number + ' = ' + result;
+//     console.log(muliplicationTables);
+// }
+
+var letter = 'a';
+
+if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+    console.log(letter, 'is bowel')
+}
+else {
+    console.log(letter, 'is not vowel');
+}
